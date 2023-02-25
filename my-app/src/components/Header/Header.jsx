57fc90cc2d623d6357f5f9e9d5cf2a5472081d2c
Olesya <olesyapetryk@gmail.com>
2023-02-25 13:41:49 +0200

    Ex-2

---
+++ my-app/src/components/Header/Header.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import logo from "./logo192.png"
+import "./Header.scss"
+
+const Header = () => {
+   const [showMenu, setShowMenu] = useState(false);
+   const [title, setTitle] = useState("Login");
+   const handleMenuToggle = () => {
+      setShowMenu(!showMenu);
+   }
+   const changeTitle = () => {
+      setTitle("Relogin");
+   }
+   return (
+      <div>
+         <header className="header">
+            <div className="header-container">
+               <div className="logo-container">
+                  <img src={logo} alt="Logo" className="logo" />
+               </div>
+               <nav className={showMenu ? "nav nav-show" : "nav"}>
+                  <ul className="nav-list">
+                     <li className="nav-item">Home</li>
+                     <li className="nav-item">Shop</li>
+                     <li className="nav-item">Contact</li>
+                     <li className="nav-item">About</li>
+                  </ul>
+               </nav>
+               <div className="login-container">
+                  <button className="login-button" onClick={() => changeTitle()}>Login</button>
+               </div>
+               <div className="burger-container" onClick={handleMenuToggle}>
+                  <div className="burger-line"></div>
+                  <div className="burger-line"></div>
+                  <div className="burger-line"></div>
+               </div>
+            </div>
+         </header>
+         <h3>{title}</h3>
+      </div>
+   )
+}
+export default Header;
